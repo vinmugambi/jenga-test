efa5f185062b5b5e10da0cdf4eb8d320c2eb508d
vinmugambi <vinmugambi17@gmail.com>
2021-08-23 10:49:25 +0300

    init

---
+++ app.js
@@ -0,0 +1,25 @@
+const http = require("http");
+const { queryAccountBalance } = require(".");
+
+const hostname = "127.0.0.1";
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+  res.setHeader("Content-Type", "text/plain");
+
+  queryAccountBalance().then(
+    function (data) {
+      res.statusCode = 200;
+      res.end(data);
+    },
+    function (error) {
+      res.statusCode = 500;
+      console.log(error);
+      res.end("server error")
+    }
+  );
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
---
+++ index.js
@@ -0,0 +1,102 @@
+require("dotenv").config();
+const axios = require("axios");
+const qs = require("querystring");
+const crypto = require("crypto");
+const fs = require("fs");
+
+function concatenate(requestData) {
+  let str = "";
+  if (requestData.accountId) str = str.concat(requestData.accountId);
+  if (requestData.countryCode) str = str.concat(requestData.countryCode);
+  return str;
+}
+
+function sign(message) {
+  const sign = crypto.createSign("SHA256");
+
+  sign.write(message);
+  sign.end();
+
+  const key = fs.readFileSync("privatekey.pem");
+  signature_b64 = sign.sign(key, "base64");
+  return signature_b64;
+}
+
+async function generateJengaBearerToken() {
+  let creds = {
+    username: process.env.JENGA_USERNAME,
+    password: process.env.JENGA_PASSWORD,
+  };
+  let token;
+  try {
+    token = await axios({
+      url: "https://uat.jengahq.io/identity/v2/token",
+      method: "POST",
+      headers: {
+        common: {
+          Authorization: "Basic " + process.env.JENGA_API_KEY,
+        },
+      },
+      data: qs.stringify(creds),
+    });
+  } catch (error) {
+    if (error.response) {
+      console.log(error.response.data);
+      // console.log(error.response.status);
+      // console.log(error.response.headers);
+    } else if (error.request) {
+      console.log(error.request);
+    } else {
+      console.log("Error", error.message);
+    }
+  }
+
+  if (token) {
+    return token.data.access_token;
+  }
+}
+
+var balanceRequest = {
+  countryCode: "KE",
+  accountId: process.env.JENGA_ACCOUNT_ID.toString(),
+};
+
+const auth = {
+  token: "",
+  async getToken() {
+    if (!this.token) {
+      let token = await generateJengaBearerToken();
+      this.token = token;
+    }
+    return this.token;
+  },
+};
+
+exports.queryAccountBalance = async function () {
+  // this works just well
+  let bearer = await auth.getToken();
+  let balance;
+  try {
+    balance = await axios({
+      url:
+        "https://uat.jengahq.io/account-test/v2/accounts/ministatement/KE/" +
+        process.env.JENGA_ACCOUNT_ID,
+      headers: {
+        common: {
+          Authorization: "Bearer " + bearer,
+          signature: sign(concatenate(balanceRequest)),
+        },
+      },
+    });
+  } catch (error) {
+    if (error.response) {
+      return Promise.reject({ type: "ResponseError", error: error.toJSON() });
+    } else if (error.request) {
+      return Promise.reject({ type: "RequestError", error: error.toJSON() });
+    } else {
+      return Promise.reject({ type: "UnknownError", error: error.message });
+    }
+  }
+
+  return balance;
+};
